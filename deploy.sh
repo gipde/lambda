80651b97903f38d8a8cb0057ef21d08edb569e06
Werner Schneider <werner@schneidernet.com>
2018-12-27 23:44:53 +0100

    java fix

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 ZIPFILE=lambda_handler.zip
 FNAME=lambda_handler_java
-javac -source 1.8 -target 1.8 *.java
-zip $ZIPFILE *.class
+./gradlew assemble
+(cd build/classes/java/main && zip -r - *) > $ZIPFILE
 
 # Handler = Filename.js + FN-Name
 # handler lambdfa_handler.call
 
 if ! aws lambda get-function --function-name $FNAME 1> /dev/null 2>&1;  then
     echo "Create Function $FNMAE..."
-    aws lambda create-function      --function-name $FNAME --zip-file fileb://./$ZIPFILE --runtime java8 --role arn:aws:iam::854014537498:role/lambda_basic_execution --handler Handler::handleRequest
+    aws lambda create-function      --function-name $FNAME --zip-file fileb://./$ZIPFILE --runtime java8 --role arn:aws:iam::854014537498:role/lambda_basic_execution --handler com.schneidernet.Handler::handleRequest
 else
     echo "Update Function $FNMAE..."
     aws lambda update-function-code --function-name $FNAME --zip-file fileb://./$ZIPFILE
