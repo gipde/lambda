ecad5d4c0cf932c271ae53c7336fc8fc6a3f118c
Werner Schneider <werner@schneidernet.com>
2018-12-27 22:43:40 +0100

    works

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.call = async function(event, context) {
     console.log("value1 = " + event.key1);
     console.log("value2 = " + event.key2);  
-    return "some success message werner";
+    return "some success message werner - it works";
     // or 
     // throw new Error("some error type"); 
  } 
