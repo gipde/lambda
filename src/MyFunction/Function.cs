4f5c46a6aee478c0f8fc35cf9cb4319a15d0d72d
Werner Schneider <werner@schneidernet.com>
2019-01-24 22:39:19 +0100

    all dotnet

--- src/MyFunction/Function.cs
+++ src/MyFunction/Function.cs
@@ -4,24 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
+
 namespace MyFunction
 {
+
+    public class Person
+    {
+        string Name;
+        private string v;
+
+        public Person(string v)
+        {
+            this.v = v;
+        }
+        override public string ToString() => v;
+
+    }
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public string FunctionHandler(Person input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            return input.ToString();
         }
     }
 }
--- test/MyFunction.Tests/FunctionTest.cs
+++ test/MyFunction.Tests/FunctionTest.cs
@@ -20,7 +20,7 @@ namespace MyFunction.Tests
             // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+            var upperCase = function.FunctionHandler(new Person("Werner"), context);
 
             Assert.Equal("HELLO WORLD", upperCase);
         }
